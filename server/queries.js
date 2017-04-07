ee9bc0891ba0071a36117dedf8b9bb794342c82a
joannawheeler <joannachristinewheeler@gmail.com>
2017-04-07 14:17:53 -0700

    deleted node server

--- server/queries.js
+++ server/queries.js
@@ -5,7 +5,9 @@ var options = {
   promiseLib: promise
 };
 
+//established connection between localhost and our RDS instance
 var pgp = require('pg-promise')(options);
+
 var connectionString = {
   user: 'teammerj',
   database: 'merjDB',
@@ -13,10 +15,12 @@ var connectionString = {
   host: 'teammerj.ccfvuax37sga.us-west-2.rds.amazonaws.com:5432',
   port: 5432
 };
+
 var db = pgp(connectionString);
 
-// add query functions
 
+
+// add query functions
 module.exports = {
 //something: something
 };
