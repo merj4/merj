0d853d14a8a3f94e47e1e9c923d9eaf0d4457a15
Rochelle Valdez <rochellevee@gmail.com>
2017-04-07 19:47:31 -0700

    Database setup

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ var bodyParser = require('body-parser')
 var path = require('path')
 var app = express()
 
+var db = require('./server/db')
+
 var port = process.env.PORT || 8080
 app.listen(port)
-console.log('listening to port', port)
+console.log('listening to portato', port)
 
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, '/client')))
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('/', function(req, res) {
+  console.log(db.User)
+    db.User.findAll({})
+      .then(function(data) {
+        console.log('inside findAll', data);
+        res.status(200).json(data);
+      });
+})
