981ad4f2bb7f3b4a7988c57f0851fb7d1a2d1e46
Rochelle Valdez <rochellevee@gmail.com>
2017-04-08 13:50:45 -0700

    added webpack.config and .babelrc files

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,12 +3,25 @@ var db = require('../db.js');
 
 module.exports = {
 
-  gerUser: function(req, res) {
-
+  getUser: function(req, res) {
+    db.User.findOne({
+      where: { id: Number(req.params.id) }
+    })
+    .then(function(user) {
+      res.status(200).json(user);
+    })
   },
 
   postUser: function(req, res) {
+    var params = {
+      username: req.body.username,
+      email: req.body.email
+    }
 
+    db.User.create(params)
+    .then(function(user) {
+      res.status(201).json(user);
+    })
   }
 
 }
