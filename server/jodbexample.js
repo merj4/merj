422bdbee871ec14f179bceb45ca180078880b4ed
Joanna Wheeler <joannachristinewheeler@gmail.com>
2017-04-07 17:18:20 -0700

    added files to integrate with sequelize tables once theyre created (#24)
    * deleted node server
    
    * created examples to connect sequel to aws successfully

---
+++ server/jodbexample.js
@@ -0,0 +1,49 @@
+var Sequelize = require('sequelize')
+
+//intialize sequelize with postgres remote url
+var sequelize = new Sequelize('postgres://teammerj:teammerjgirls@teammerj.ccfvuax37sga.us-west-2.rds.amazonaws.com:5432/merjDB');
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database', err);
+  })
+
+var User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+});
+
+// force: true will drop the table if it already exists
+User.sync({force: true}).then(function () {
+  // Table created
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+});
+
+User.findAll().then(function(users) {
+  console.log(users)
+})
+
+module.exports = {
+  User: User,
+}
+
+//create User table
+// var User = db.define('User', {
+//   username: Sequelize.STRING,
+//   email: Sequelize.STRING
+// })
+
+// var Event = db.define('Event', {
+
+// })
--- server/joexampleserver.js
+++ server/joexampleserver.js
@@ -12,3 +12,12 @@ console.log('listening to port', port)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '/client')))
+
+app.get('/', function(req, res) {
+  console.log(db.User)
+    db.User.findAll({})
+      .then(function(data) {
+        console.log('inside findAll', data);
+        res.status(200).json(data);
+      });
+})
