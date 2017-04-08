4144275d497b9fb4cc43a9cf37f5a5a91d92eb7b
Rochelle Valdez <rochellevee@gmail.com>
2017-04-08 15:57:33 -0700

    Dropped database and recreated tables

--- server/controllers/chat-controller.js
+++ server/controllers/chat-controller.js
@@ -4,7 +4,19 @@ var db = require('../db.js');
 module.exports = {
 
   getMessages: function(req, res) {
-
+    db.Message.findAll({
+        limit: 50,
+        order: '"createdAt" DESC'
+    })
+    .then(function(messages) {
+      messages = messages.map(function(message) {
+        var messageData = message.dataValues;
+        return {
+          message: messageData.message
+        }
+      });
+      res.status(200).json(messages);
+    })
   },
 
   postMessage: function(req, res) {
