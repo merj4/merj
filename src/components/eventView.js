ab083141a27304f9a7b24ae598ff6858a84f5672
Emma Rios <tigersoundtracks@gmail.com>
2017-04-08 17:37:21 -0600

    Completed setup for frontend React framework

---
+++ src/components/eventView.js
@@ -0,0 +1,8 @@
+// every class must have a render function
+  // it's recommended that you start with a functional based component and
+  // only refactor to a class when you need some added functionality
+
+  // This is where a single event view will be managed
+    // I imagine this will need to be a class component because it will need to handle changing state from the chat controller
+
+  // Chat will also be passed into this so it can be rendered, too
---
+++ src/components/profile.js
@@ -0,0 +1,5 @@
+// every class must have a render function
+  // it's recommended that you start with a functional based component and
+  // only refactor to a class when you need some added functionality
+
+  // We should get authentication implemented before we work this out
