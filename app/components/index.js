5438ce6012101220ece501b5221371315b1e3bbb
Rochelle Valdez <rochellevee@gmail.com>
2017-04-10 14:45:25 -0700

    Frontend (#38)
    * getMessages
    
    * Fixed db relations error
    
    * Added karma config, test folder, and sample tests
    
    * Added ajax request
    
    * Changed all files to jsx
    
    * Moved Bootstrap CDN
    
    * Gutted create react app and started from scratch
    
    * Moved indexJS to app folder

---
+++ app/components/index.js
@@ -0,0 +1,48 @@
+console.log('Hello')
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './header.js';
+import Search from './search';
+import Filter from './filter';
+import EventList from './eventList';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: []
+    }
+
+    this.componentDidMount = this.componentDidMount.bind(this)
+
+  }
+
+  componentDidMount() {
+    var myInit = {
+      method: "GET"
+    };
+
+    fetch("/", myInit).then((response) => {
+      return response.json();
+    }).then((data) => {
+      this.state.events = data;
+      this.setState(this.state);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Search />
+        <Filter />
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var app = express()
 var db = require('./server/db')
 
 //PORT
-var db = require('./server/db')
-
 var port = process.env.PORT || 8080
 app.listen(port)
 console.log('listening to portato', port)
@@ -15,13 +13,7 @@ console.log('listening to portato', port)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '/app')))
-
-app.get('/', (req, res) => {
-  res.send();
-})
-
-require('./server/routes')(app, express)
-
+require('./server/routes')(app, express);
 
 module.exports = {
   app: app
