19049830066ac8c7a979a466cc68a99590891645
Rochelle Valdez <rochellevee@gmail.com>
2017-04-09 19:18:13 -0700

    Added karma config, test folder, and sample tests

---
+++ karma.conf.js
@@ -0,0 +1,53 @@
+const path = require('path');
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    browsers: ['PhantomJS'],
+    frameworks: ['mocha'],
+    files: [
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      'test/**/*.js'
+    ],
+    preprocessors: {
+      'src/public/AppEntry.js': ['webpack', 'sourcemap'],
+      'test/**/*.js': ['webpack', 'sourcemap']
+    },
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: path.resolve(__dirname, 'node_modules'),
+            query: {
+              presets: ['react', 'es2015',]
+            }
+          },
+          {
+            test: /\.json$/,
+            loader: 'json-loader',
+          },
+        ]
+      },
+      externals: {
+        'react/addons': true,
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
+      }
+    },
+    webpackServer: {
+      noInfo: true
+    },
+    reporters: ['nyan'],
+    nyanReporter: {
+      suppressErrorHighlighting: true,
+    },
+    
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: false,
+  });
+};
