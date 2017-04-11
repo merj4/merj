04c382a9f2f8bfd450d47b70a3567cc4e95c8118
Rochelle Valdez <rochellevee@gmail.com>
2017-04-10 23:43:58 -0700

    Added more photos

--- events.js
+++ events.js
@@ -43,6 +43,52 @@ var exampleEvents = [{
   category:"Outdoor",
   description:"Come out to the beach and enjoy company, food and sand castles",
   image:"http://www.loadednightclub.co.uk/wp-content/uploads/sites/2/2015/05/4dadc304234cad1821315e885c04c01d.jpg"
+},
+{
+  location:"Santa Monica, CA",
+  date:"Aug. 10, 2017",
+  title:"Fun in the Sun",
+  time:"2:00 pm",
+  category:"Outdoor",
+  description:"Come out to the beach and enjoy company, food and sand castles",
+  image:"http://www.unique-canvas.com/media/images/popup/meer-und-traumstraende-fotografie--fotomotiv-beach-party--816406.jpg"
+},
+
+{
+  location:"Oakland, CA",
+  date:"Aug. 10, 2017",
+  title:"Fun in the Sun",
+  time:"2:00 pm",
+  category:"Outdoor",
+  description:"Get rekt. Hype. Bonfire. Beach. Drinks. Food.",
+  image:"http://media.new.mensxp.com/media/content/2015/May/wildestbeachpartydestinationsintheworldforsinglemen0_1431689937_980x457.jpg"
+},
+{
+  location:"San Diego, CA",
+  date:"Aug. 10, 2017",
+  title:"Fun in the Sun",
+  time:"2:00 pm",
+  category:"Outdoor",
+  description:"Come out to the beach and enjoy company, food and sand castles",
+  image:"http://cdn.sosueme.ie/wp-content/uploads/2016/01/1_best_party_beach_South_Beach_Miami_Florida-e1399908931396.jpg"
+},
+{
+  location:"Yosemite, CA",
+  date:"Aug. 10, 2017",
+  title:"Fun in the Sun",
+  time:"2:00 pm",
+  category:"Outdoor",
+  description: "Be free, be young, be Malibu.",
+  image: "http://www.winterparty.com/sites/www.winterparty.com/files/styles/project_1126_470/public/BeachPartySlider1_0.jpg?itok=5eB_59TL"
+},
+{
+  location:"Lake Tahoe, CA",
+  date:"Aug. 10, 2017",
+  title:"Fun in the Sun",
+  time:"2:00 pm",
+  category:"Outdoor",
+  description:"Come out to the beach and enjoy company, food and sand castles",
+  image:"https://s-media-cache-ak0.pinimg.com/originals/62/f0/48/62f04865010d469b7f44dac815d916df.jpg"
 }]
 
 export {exampleEvents};
