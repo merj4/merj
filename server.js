b409fcdaa4a7632f88bb3408732f21c636dd8be1
Rochelle Valdez <rochellevee@gmail.com>
2017-04-10 17:04:53 -0700

    Added webpack-dev-server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser')
 var path = require('path')
 var app = express()
 var db = require('./server/db')
+var morgan = require('morgan');
+
 
 //PORT
 var port = process.env.PORT || 8080
@@ -10,9 +12,10 @@ app.listen(port)
 console.log('listening to portato', port)
 
 //MIDDLEWARE
+app.use(morgan('dev'));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, '/app')))
+app.use(express.static(path.join(__dirname, '/app/dist')))
 require('./server/routes')(app, express);
 
 module.exports = {
