fd984cbfab5b6428479e547b6a6f272c3c352b92
joannawheeler <joannachristinewheeler@gmail.com>
2017-04-11 17:53:48 -0700

    ..

--- app/components/search.js
+++ app/components/search.js
@@ -5,16 +5,17 @@ import { Button, Col, Row } from 'react-bootstrap';
   // it's recommended that you start with a functional based component and
   // only refactor to a class when you need some added functionality
 
-const Search =() => {
-    return (
-      <div className="search-bar">
-        <Row>
-          <Col xs={12}>
-            <input className="centered" />
-          </Col>
-        </Row>
-      </div>
-    );
-}
+
+
+const Search = React.createClass({
+    doSearch:function(){
+        var query=this.refs.searchInput.getDOMNode().value; // this is the search text
+        this.props.doSearch(query);
+    },
+    render:function(){
+        return <input type="text" ref="searchInput" placeholder="Search" value={this.props.query} onChange={this.doSearch}/>
+    }
+});
+
 
 export default Search;
