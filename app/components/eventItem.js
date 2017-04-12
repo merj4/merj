3f059d259e0f304ab061b44a0bac1e38bfeed004
Rochelle Valdez <rochellevee@gmail.com>
2017-04-12 10:54:57 -0700

    adding jquery to pull data from form

--- app/components/eventItem.js
+++ app/components/eventItem.js
@@ -1,22 +1,12 @@
 import React, {Component} from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui';
 import FlatButton from 'material-ui/FlatButton';
-// every class must have a render function
-  // it's recommended that you start with a functional based component and
-  // only refactor to a class when you need some added functionality
-
-  // This component controls rendering one event based on its info from the database
-
-  // I think this will then get passed into our eventsList so multiple events can be rendered on that view
-
-  // Let's try and get some dummy data from the database setup so we can use it here?
-
-    // The info we need from the database are event name, place, date, number of people attending (do we still have that in our DB?), the latest message(s) from the event chat and the most recent 1-2 images from the chat.
 
 class EventItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      openEvent: false
     };
   }
 
