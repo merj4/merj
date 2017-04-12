358bb4f40a1e69caf6e47f8c906dec354de9251b
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2017-04-12 02:59:56 -0700

    made button for map or list

--- app/components/mapView.js
+++ app/components/mapView.js
@@ -1,6 +1,27 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
 // every class must have a render function
   // it's recommended that you start with a functional based component and
   // only refactor to a class when you need some added functionality
 
   // Implement AFTER MVP - don't even think about it!
+class MapView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        hello world!
+      </div>
+    )
+  }
+};
+
+export default MapView;
+
   
