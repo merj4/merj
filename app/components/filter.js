4d65564027056ba0f502fe229294b2588bde3c09
Emma Rios <tigersoundtracks@gmail.com>
2017-04-12 14:48:45 -0600

    Cleaned up filter bar implimentation

--- app/components/filter.js
+++ app/components/filter.js
@@ -20,66 +20,18 @@ const TabsExampleSimple = () => (
       <div style={styles.headline}></div>
     </Tab>
     <Tab label="Size" >
-      <div>
-        <h2 style={styles.headline}>Size</h2>
-        <p>
-          This is another example tab.
-        </p>
-      </div>
+      <div style={styles.headline}></div>
     </Tab>
     <Tab label="Date" >
-      <div>
-        <h2 style={styles.headline}>Date</h2>
-        <p>
-          This is another example tab.
-        </p>
-      </div>
+      <div style={styles.headline}></div>
     </Tab>
     <Tab label="Now" >
-      <div>
-        <h2 style={styles.headline}>Now</h2>
-        <p>
-          This is another example tab.
-        </p>
-      </div>
+      <div style={styles.headline}></div>
     </Tab>
     <Tab label="Hot" >
-      <div>
-        <h2 style={styles.headline}>Hot</h2>
-        <p>
-          This is another example tab.
-        </p>
-      </div>
+      <div style={styles.headline}></div>
     </Tab>
   </Tabs>
 );
 
 export default TabsExampleSimple;
-
-// import React from 'react';
-// import { Button, Col, Row } from 'react-bootstrap';
-
-// // every class must have a render function
-//   // it's recommended that you start with a functional based component and
-//   // only refactor to a class when you need some added functionality
-
-// // this header will contain our app name in the center, which will also be a clickable link to go to the homepage/eventsList
-
-// // on the right side of the header, we need a + button to add an event
-
-// const Filter = () => {
-//   return (
-//     <div>
-//       <Row>
-//         <Col xs={2} className="text-center">Distance</Col>
-//         <Col xs={2} className="text-center">Category</Col>
-//         <Col xs={2} className="text-center">Date</Col>
-//         <Col xs={2} className="text-center">Now</Col>
-//         <Col xs={2} className="text-center">Hot</Col>
-//         <Col xs={2} className="text-center"><Button className="map-view">Map</Button></Col>
-//       </Row>
-//     </div>
-//   );
-// }
-
-// export default Filter;
