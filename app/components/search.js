0688c076b9c6d95e5c0124580f76b02871489a82
Rochelle Valdez <rochellevee@gmail.com>
2017-04-13 11:09:18 -0700

    Added search and formatted time

--- app/components/search.js
+++ app/components/search.js
@@ -2,12 +2,26 @@ import React from 'react';
 import AutoComplete from 'material-ui/AutoComplete';
 import _ from 'underscore';
 
+let transform = (date) => {
+  let time = new Date(date + 'UTC');
+  return time.toString()
+}
 
 const Search = (props) => {
   let data = props.data;
   let store = [];
-  let keys = _.each(data, (obj) => 
-    _.each(obj, (key) => store.push(key)))
+  let keys = _.each(data, function(obj) {
+     _.each(obj, function(value, key) {
+      if (key !== "image") {
+        if (key === "time") {
+        transform(obj[key])
+        
+        }
+        store.push(value) 
+      }
+    })
+  })
+     
 
 
   return (
