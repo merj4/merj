85cd63e3bbe90fe6548ac4c99d0e5284cbf6b041
Emma Rios <tigersoundtracks@gmail.com>
2017-04-12 22:16:20 -0600

    still working on trying to implement map into filter bar

--- app/components/filter.js
+++ app/components/filter.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
+import {EventItem} from './eventItem';
+import {EventList} from './eventList';
+import MapView from './mapView';
+import ListOrMap from './ListOrMap';
 
 const styles = {
     headline: {
@@ -10,30 +14,45 @@ const styles = {
   },
 };
 
-const TabsExampleSimple = () => (
-  <Tabs>
-    <Tab label="Distance"  >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Category" >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Size" >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Date" >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Now" >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Hot" >
-      <div style={styles.headline}> </div>
-    </Tab>
-    <Tab label="Map" >
-      <div style={styles.headline}> </div>
-    </Tab>
-  </Tabs>
-);
+export default class Filter extends React.Component {
 
-export default TabsExampleSimple;
+  constructor(props) {
+    super(props);
+    this.state = {
+      // isMap : false,
+    }
+  }
+
+  render() {
+    return (
+      <Tabs
+        // value={this.state.value}
+        // onChange={this.handleChange}
+      >
+        <Tab label="Distance"  >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Category" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Size" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Date" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Now" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Hot" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Map">
+          <div style={styles.headline}>
+            <ListOrMap events={this.props.events} />
+          </div>
+        </Tab>
+      </Tabs>
+    );
+  }
+}
