bb70643215478219478f692c14543ab5c1774a53
Misaki <misaki.matsumoto2524@gmail.com>
2017-04-12 21:14:51 -0700

    added google map (#76)
    * commiting for merge
    
    * commiting for merge
    
    * cleaning up to merge
    
    * made button for map or list
    
    * functionally map or list button working
    
    * added toggle list and map with burron
    
    * modified css font for modal
    
    * added map
    
    * adding google map display

--- app/components/filter.js
+++ app/components/filter.js
@@ -11,50 +11,28 @@ const styles = {
   },
 };
 
-export default class Filter extends React.Component {
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: 'a',
-  //   };
-  // }
-
-  // handleChange = (value) => {
-  //   this.setState({
-  //     value: value,
-  //   });
-  // };
-
-  render() {
-    return (
-      <Tabs
-        // value={this.state.value}
-        // onChange={this.handleChange}
-      >
-        <Tab label="Distance"  >
-          <div style={styles.headline}></div>
-        </Tab>
-        <Tab label="Category" >
-          <div style={styles.headline}></div>
-        </Tab>
-        <Tab label="Size" >
-          <div style={styles.headline}></div>
-        </Tab>
-        <Tab label="Date" >
-          <div style={styles.headline}></div>
-        </Tab>
-        <Tab label="Now" >
-          <div style={styles.headline}></div>
-        </Tab>
-        <Tab label="Hot" >
-          <div style={styles.headline}></div>
-        </Tab>
-        <Tab label="Map" >
-          <div style={styles.headline}>
-          </div>
-        </Tab>
-      </Tabs>
-    );
-  }
-}
+const TabsExampleSimple = () => (
+  <Tabs>
+    <Tab label="Distance"  >
+      <div style={styles.headline}></div>
+    </Tab>
+    <Tab label="Category" >
+      <div style={styles.headline}></div>
+    </Tab>
+    <Tab label="Size" >
+      <div style={styles.headline}></div>
+    </Tab>
+    <Tab label="Date" >
+      <div style={styles.headline}></div>
+    </Tab>
+    <Tab label="Now" >
+      <div style={styles.headline}></div>
+    </Tab>
+    <Tab label="Hot" >
+      <div style={styles.headline}></div>
+    </Tab>
+    <Tab label="Map" >
+      <div style={styles.headline}></div>
+    </Tab>
+  </Tabs>
+);
