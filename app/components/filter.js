baa5bb3cb98e61f1b9b0886518586aeea755b756
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2017-04-12 21:17:41 -0700

    added css

--- app/components/filter.js
+++ app/components/filter.js
@@ -11,28 +11,50 @@ const styles = {
   },
 };
 
-const TabsExampleSimple = () => (
-  <Tabs>
-    <Tab label="Distance"  >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Category" >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Size" >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Date" >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Now" >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Hot" >
-      <div style={styles.headline}></div>
-    </Tab>
-    <Tab label="Map" >
-      <div style={styles.headline}></div>
-    </Tab>
-  </Tabs>
-);
+export default class Filter extends React.Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: 'a',
+  //   };
+  // }
+
+  // handleChange = (value) => {
+  //   this.setState({
+  //     value: value,
+  //   });
+  // };
+
+  render() {
+    return (
+      <Tabs
+        // value={this.state.value}
+        // onChange={this.handleChange}
+      >
+        <Tab label="Distance"  >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Category" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Size" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Date" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Now" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Hot" >
+          <div style={styles.headline}></div>
+        </Tab>
+        <Tab label="Map" >
+          <div style={styles.headline}>
+          </div>
+        </Tab>
+      </Tabs>
+    );
+  }
+}
