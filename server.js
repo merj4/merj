5440440fb1510f9b86cf35a757565e2f0a2b6740
Rochelle Valdez <rochellevee@gmail.com>
2017-04-17 12:07:26 -0700

    Added server side sockets for connect and disconnect

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ var path = require('path')
 var app = express()
 var db = require('./server/db')
 var morgan = require('morgan');
+//SOCKET requires
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socet.io').listen(server);
+//SOCKET
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    io.emit('message', message);
+  })
+})
 
+socket.on('disconnect', () => {
+  console.log('A user disconnected');
+})
 
 //PORT
 var port = process.env.PORT || 8080
@@ -17,6 +30,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '/app/dist')))
 require('./server/routes')(app, express);
+
 
 module.exports = {
   app: app
