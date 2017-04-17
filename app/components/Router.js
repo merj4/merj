479d4f629831791ce88ee97e9c12ad1a0fa2104a
Rochelle Valdez <rochellevee@gmail.com>
2017-04-16 20:13:14 -0700

    Attempting to integrate react router

--- app/components/Router.js
+++ app/components/Router.js
@@ -1,57 +1,24 @@
-import React from 'react'
+import React from 'react';
 import {
   BrowserRouter as Router,
-  Route,
+  Route, 
   Link
-} from 'react-router-dom'
-import Mapview from './mapView'
-import EventList from './eventList'
-import TabsExampleSimple from './filter'
-import {Tabs, Tab} from 'material-ui/Tabs';
-import Slider from 'material-ui/Slider';
+} from 'react-router-dom';
 
-class Nav extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      mapView: false,
-      listView: true
-    };
-  }
+const Router = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/event">Event</Link></li>
+      </ul>
 
-  mapSet() {
-    this.setState({
-      mapButton: true,
-      listButton: false
-    })
-  }
+      <hr />
 
-  listSet() {
-    this.setState({
-      mapButton: false,
-      listButton: true
-    })
-  }
+      <Route exact path="/" component={App} />
+      <Route path="/event" component={Event} />
+    </div>
+  </Router>
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <Link to='/map'>
-            <TabsExampleSimple buttonValue={this.state.mapButton}
-            mapChange={this.mapSet.bind(this)}/>
-          </Link>
-          <Link to='/list'>
-            <ListButton buttonValue={this.state.listButton}
-            listChange={this.listSet.bind(this)}/>
-          </Link>
 
-          <Route exact path='/map' component={ListOrMap}/>
-          <Route exact path='/list' component={ListView}/>
-        </div>
-      </Router>
-    )
-  }
-}
-
-export default CatNav
+)
