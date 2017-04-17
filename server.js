fa6283aca11dacf45ea139d6c158e52384b24c6c
Rochelle Valdez <rochellevee@gmail.com>
2017-04-17 13:08:47 -0700

    Configured backend configuration for webpack to work with socket.io

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ var morgan = require('morgan');
 //SOCKET requires
 var http = require('http');
 var server = http.createServer(app);
-var io = require('socet.io').listen(server);
-//SOCKET
+var io = require('socket.io')(server);
+
+//PORT
+var port = process.env.PORT || 8080
+app.listen(port)
+console.log('listening to portato', port)
+// SOCKET
 io.on('connection', (socket) => {
   socket.on('message', (message) => {
     io.emit('message', message);
   })
 })
 
-socket.on('disconnect', () => {
+io.on('disconnect', () => {
   console.log('A user disconnected');
 })
-
-//PORT
-var port = process.env.PORT || 8080
-app.listen(port)
-console.log('listening to portato', port)
 
 //MIDDLEWARE
 app.use(morgan('dev'));
