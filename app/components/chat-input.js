2e0e720c60f04c45af66ef04ba6e3748852d6ce6
Rochelle Valdez <rochellevee@gmail.com>
2017-04-18 13:34:18 -0700

    Fixing build errors

--- app/components/chat-input.js
+++ app/components/chat-input.js
@@ -2,21 +2,10 @@ import React, { Component } from 'react';
 import io from 'socket.io-client';
 import jquery from 'jquery';
 
-$(function () {
-  var socket = io();
-  $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-  socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
-  });
-});
+
 const ChatInput = () => {
   return (
   <div>
-   <div className="container"></div>
     <ul id="messages"></ul>
     <form action="">
       <input id="m" autocomplete="off" /><button>Send</button>
