db4dac1ddfd284ea707b4f72c29d1c5108d26b82
Rochelle Valdez <rochellevee@gmail.com>
2017-04-18 13:02:45 -0700

    Moving chat input to its own component from the index.html file

--- app/components/eventView.js
+++ app/components/eventView.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ChatUsers } from './chat-users'
-import { ChatMessage } from './chat-message'
 import { ChatInput } from './chat-input'
 import { ChatContainer } from './chat-container'
 
 const EventView = (props) => (
   <div>
     <ChatUsers />
+    <EventDetails events={props.events}/>
+    <ChatContainer />
+    <ChatInput />
   </div>
 );
 
