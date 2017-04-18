77f33c52419381293b98d1338f327e3dae6c2553
Rochelle Valdez <rochellevee@gmail.com>
2017-04-18 12:27:58 -0700

    Formatting new chat component pages

--- app/components/chat-container.jsx
+++ app/components/chat-container.jsx
@@ -1,6 +1,2 @@
-// every class must have a render function
-  // it's recommended that you start with a functional based component and
-  // only refactor to a class when you need some added functionality
+import { ChatMessage } from './chat-message'
 
-  // This component will control the chat, which we may use socket.io for
-  // This (probably class component) component will be passed into the eventView controller
