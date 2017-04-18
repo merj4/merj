50fe14f29ae942e39565bcddd9e4d7881c34ee04
Rochelle Valdez <rochellevee@gmail.com>
2017-04-18 13:11:10 -0700

    Fixing build errors

--- app/components/chat-eventDetails.js
+++ app/components/chat-eventDetails.js
@@ -1,10 +1,8 @@
-// every class must have a render function
-  // it's recommended that you start with a functional based component and
-  // only refactor to a class when you need some added functionality
+import React, { Component } from 'react';
+import { ChatMessage } from './chat-message';
 
-  // This component strictly handles getting info from the database and rendering it
-  // This gets passed into eventView (along with chat) to create the complete eventView
+const ChatContainer = () => (
 
-  // The DB info that will get rendered here is the event name, place and host name
+)
 
-  // This probably doesn't need to handle any changing state and could just be a regular component
+export { ChatContainer }
--- app/components/eventView.js
+++ app/components/eventView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ChatUsers } from './chat-users'
 import { ChatInput } from './chat-input'
 import { ChatContainer } from './chat-container'
+import { EventDetails } from './chat-eventDetails'
 
 const EventView = (props) => (
   <div>
