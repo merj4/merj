6aa45d13c023b2635e172dbaad2a7379cf51af19
Rochelle Valdez <rochellevee@gmail.com>
2017-04-18 13:07:22 -0700

    Added ChatContainer component

---
+++ app/components/chat-container.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { ChatMessage } from './chat-message'
+
+let MobileTearSheet = React.createClass({
+
+  propTypes: {
+    height: React.PropTypes.number
+  },
+
+  getDefaultProps() {
+    return {
+      height: 775
+    };
+  },
+
+  render() {
+
+    let styles = {
+      root: {
+        float: 'right',
+        marginBottom: 24,
+        marginRight: 24,
+        width: 700
+
+      },
+
+      container: {
+        border: 'solid 1px #d9d9d9',
+        height: this.props.height,
+        overflow: 'hidden'
+      }
+    };
+
+    return (
+      <div style={styles.root}>
+        <div style={styles.container}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+
+});
+
+
+
+const ChatContainer = (props) => (
+
+  <div>
+    <MobileTearSheet>
+      <ChatMessage message={props.message}/>
+    </MobileTearSheet>
+  </div>
+);
+
+export { ChatContainer }
