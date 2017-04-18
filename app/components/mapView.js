5d7dcf7e14f25ce7b4c3e827e15d13eed2cc7f55
Misaki <misaki.matsumoto2524@gmail.com>
2017-04-17 17:41:57 -0700

    Added markers and infowindows on map view using google map api (#101)
    * merging
    
    * added google map infowindow
    
    * getevent location
    
    * added markers and infowindows on google map

--- app/components/mapView.js
+++ app/components/mapView.js
@@ -1,26 +1,73 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {EventItem} from './eventItem';
+
+// let INITIAL_LOCATION = {
+//     address: 'London, United Kingdom',
+//     position: {
+//       latitude: 51.5085300,
+//       longitude: -0.1257400
+//     }
+//   }
+
+// let INITIAL_MAP_ZOOM_LEVEL = 8;
+
 
 class MapView extends Component {
-  componentDidMount() {
-    new google.maps.Map(this.refs.map, {
-      center:{
-        lat: -34.397,
-        lng: 150.644
-      },
-      zoom: 12
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
   }
-  render() {
-  const mapStyle = {
-      width: 1140,
-      height: 770,
-    };
 
-    return (
-      <div ref="map" style={mapStyle}></div>
+  // componentDidMount() {
+  //   const map = new google.maps.Map(this.refs.map, {
+  //     center:{
+  //       lat: INITIAL_LOCATION.position.latitude,
+  //       lng: INITIAL_LOCATION.position.longitude,
+  //     },
+  //     zoom: INITIAL_MAP_ZOOM_LEVEL,
+  //     mapTypeId: 'roadmap',
+
+  //   });
+
+  //   this.marker = new google.maps.Marker({
+  //     map: this.map,
+  //     position: {
+  //       lat: INITIAL_LOCATION.position.latitude,
+  //       lng: INITIAL_LOCATION.position.longitude
+  //     }
+  //   });
+
+  //   // new google.maps.Geocorder();
+  // }
+
+
+
+  // Trying to get event location and put them in locations array
+  // **but right now, it's empty array, why?
+  getEventLocation() {
+    let data = this.props.events.slice();
+    // let locations = [];
+    // data.forEach((event) => 
+    //   locations.push(this.props.event.location)
+    // )
+    // console.log('locations:', locations)
+  }
+
+
+render() {
+  const style = {
+    width: "800px",
+    height: "800px",
+  };
+  console.log("getEventLocation:", this.props.events);
+  return (
+    <div className="container">
+      <div className="map" ref="map" style={style} ></div>
+    </div>
     );
   }
 };
 
-export default MapView;
+module.exports = MapView;
