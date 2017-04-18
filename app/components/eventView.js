edc0d677d6fceb6f41e45201bb8e58b40ba13897
Rochelle Valdez <rochellevee@gmail.com>
2017-04-18 13:02:45 -0700

    Moving chat input to its own component from the index.html file

--- app/components/eventView.js
+++ app/components/eventView.js
@@ -1,44 +1,15 @@
 import React from 'react';
-import Avatar from 'material-ui/Avatar';
-import {List, ListItem} from 'material-ui/List';
-import Subheader from 'material-ui/Subheader';
-import Divider from 'material-ui/Divider';
-import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
-import {Popover} from 'react-bootstrap'
+import { ChatUsers } from './chat-users'
+import { ChatInput } from './chat-input'
+import { ChatContainer } from './chat-container'
 
-
-
-const EventView = () => (
-  <Row>
-    <List Col xs={6}>
-      <Subheader>Recent chats</Subheader>
-      <ListItem
-        primaryText="JSON.stringify(kim)"
-        leftAvatar={<Avatar src="images/jason.jpg" />}
-        rightIcon={<CommunicationChatBubble />}
-      />
-      <ListItem
-        primaryText="jDCstubbs"
-        leftAvatar={<Avatar src="images/jordan.jpg" />}
-        rightIcon={<CommunicationChatBubble />}
-      />
-      <ListItem
-        primaryText="Simon Ledger"
-        leftAvatar={<Avatar src="images/simon.jpg" />}
-        rightIcon={<CommunicationChatBubble />}
-      />
-      <ListItem
-        primaryText="J<3sOld_Bay"
-        leftAvatar={<Avatar src="images/jesse.jpg" />}
-        rightIcon={<CommunicationChatBubble />}
-      />
-      <ListItem
-        primaryText="NeverFaized"
-        leftAvatar={<Avatar src="images/faiz.jpg" />}
-        rightIcon={<CommunicationChatBubble />}
-      />
-    </List>
-  </Row>
+const EventView = (props) => (
+  <div>
+    <ChatUsers />
+    <EventDetails events={props.events}/>
+    <ChatContainer />
+    <ChatInput />
+  </div>
 );
 
 
