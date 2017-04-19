56e23211e02cd35b23b4616e7ae90123a87aac38
Misaki <misaki.matsumoto2524@gmail.com>
2017-04-19 10:44:05 -0700

    authentication (login and logout) (#110)
    * merging
    
    * added google map infowindow
    
    * getevent location
    
    * pushing change for mapview
    
    * cleaning up for merging
    
    * merging
    
    * added authentication and logout button

--- app/components/login.js
+++ app/components/login.js
@@ -3,3 +3,34 @@
   // only refactor to a class when you need some added functionality
 
   // to be discussed/implemented AFTER MVP - don't even bother with this
+
+import React from 'react'
+import AuthService from '../../config/AuthService.js'
+//import styles from './styles.module.css'
+
+export class Login extends React.Component {
+
+
+  render() {
+    const { auth } = this.props
+
+    return (
+      <div className="container login">
+        <div className="jumbotron">
+          <div className="row align-items-center justify-contents-center">
+            <div className="col">
+              <h1>MERJ</h1>
+            </div>
+            <div className="w-100"></div>
+            <div className="w-100"></div>
+            <div className="col">
+              <button className="btn btn-primary" onClick={auth.login.bind(this)}>Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login;
