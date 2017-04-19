cb8141fd2e7f5f1f93b5431c4f21c826dddb2007
Rochelle Valdez <rochellevee@gmail.com>
2017-04-19 11:17:08 -0700

    Adding files back in that were lost in rebase

---
+++ app/components/chat-users-container.js
@@ -0,0 +1,45 @@
+let React = require('react');
+
+
+let MobileTearSheet = React.createClass({
+
+  propTypes: {
+    height: React.PropTypes.number
+  },
+
+  getDefaultProps() {
+    return {
+      height: 350
+    };
+  },
+
+  render() {
+
+    let styles = {
+      root: {
+        float: 'left',
+        marginBottom: 24,
+        marginRight: 24,
+        width: 360
+
+      },
+
+      container: {
+        border: 'solid 1px #d9d9d9',
+        height: this.props.height,
+        overflow: 'hidden'
+      }
+    };
+
+    return (
+      <div style={styles.root}>
+        <div style={styles.container}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+
+});
+
+module.exports = MobileTearSheet;
