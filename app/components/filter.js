bde0d9328df1aca106329f21e3a6074c3309f811
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2017-04-21 23:58:57 -0700

    added alert when no events are found

--- app/components/filter.js
+++ app/components/filter.js
@@ -120,9 +120,9 @@ class Filter extends Component {
           <div style={styles.headline}>
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">Menu 1<span class="caret"></span></a>
             <ul class="dropdown-menu">
-              <li><button onClick={() => this.distanceHandler(8046.72)}>2 miles</button></li>
-              <li><button>5 miles</button></li>
-              <li><button>20 miles</button></li> 
+              <li><button onClick={() => this.distanceHandler(3218.69)}>2 miles</button></li>
+              <li><button onClick={() => this.distanceHandler(8046.72)}>5 miles</button></li>
+              <li><button onClick={() => this.distanceHandler(32186.9)}>20 miles</button></li>
             </ul>
           </div>
         </Tab>
