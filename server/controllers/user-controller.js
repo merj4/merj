1047217dfb0a87a3d6ce6610f8bc7227fce6cce7
Emma Rios <tigersoundtracks@gmail.com>
2017-04-23 08:45:26 -0600

    modified user-controller to attempt to get user id and added post req for joining event

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -4,11 +4,15 @@ var db = require('../db.js');
 module.exports = {
 
   getUser: function(req, res) {
-    console.log('GET USER REQ: ', req);
-    db.User.findOne({
-      where: { email: String(req.params.email) }
+    console.log('GET USER REQ**: ', req.body);
+    db.User.findAll({
+      where: { email: req.body.email }
     })
     .then(function(user) {
+      console.log('User Get req:', user);
+      // return {
+      //   id: user
+      // };
       res.status(200).json(user);
     });
   },
@@ -16,7 +20,24 @@ module.exports = {
   postUser: function(req, res) {
     db.User.findOrCreate({where: {email: req.body.email},
       defaults: {username: req.body.username, image: req.body.image}
-    }).spread(function(user, created) {
+    })
+    .spread(function(user, created) {
+      console.log(user.get({
+        plain: true
+      }));
+      console.log(created);
+    })
+    .then(function(user) {
+      res.status(201).json(user);
+    });
+  },
+
+  postJoinEvent: function(req, res) {
+    db.EventParticipant.findOrCreate({where:
+      {EventId: req.body.EventId},
+      include: [{UserId: req.body.UserId}]
+    })
+    .spread(function(user, created) {
       console.log(user.get({
         plain: true
       }));
@@ -24,15 +45,5 @@ module.exports = {
     }).then(function(user) {
       res.status(201).json(user);
     });
-    // var params = {
-    //   username: req.body.username,
-    //   email: req.body.email,
-    //   image: req.body.image
-    // }
-
-    // db.User.create(params)
-    // .then(function(user) {
-    //   res.status(201).json(user);
-    // })
-  }
+  },
 };
