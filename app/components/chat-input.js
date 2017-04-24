3f7eaaaa5d71997da0b2ccbd2087b3ada2fddda6
Rochelle Valdez <rochellevee@gmail.com>
2017-04-24 12:30:51 -0700

    Adding photo upload feature

--- app/components/chat-input.js
+++ app/components/chat-input.js
@@ -4,6 +4,47 @@ import io from 'socket.io-client';
 import {orange500, blue500} from 'material-ui/styles/colors';
 import moment from 'moment';
 import PhotoIcon from 'material-ui/svg-icons/image/add-a-photo'
+import { Button, Modal, Form } from 'react-bootstrap';
+import axios from 'axios';
+
+ //////CLOUDINARY UPLOAD IMAGE//////////
+  var imgPreview = document.getElementById('img-preview');
+  var imgFormPreview = document.getElementById('img-form-preview');
+
+  var CLOUDINARY_URL =  'https://api.cloudinary.com/v1_1/dcjoeciha/upload';
+  var CLOUDINARY_UPLOAD_PRESET = "ceydn5w3";
+
+  $('#url-input').on('change', function() {
+    $scope.image = $scope.addImageByUrl;
+    imgPreview.src = $scope.addImageByUrl;
+    imgFormPreview.src = $scope.addImageByUrl;
+  });
+
+
+$('#upload-input').on('change', function(event) {
+
+  var file = event.target.files[0];
+  var formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+  axios({
+    url: CLOUDINARY_URL,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: formData
+  }).then(function(res) {
+    console.log(res);
+    imgPreview.src = res.data.secure_url;
+    imgFormPreview.src = res.data.secure_url;
+    $scope.image = res.data.secure_url;
+  }).catch(function(err) {
+    console.error(err);
+  });
+});
+
 
 const styles = {
   errorStyle: {
@@ -28,10 +69,10 @@ class ChatInput extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      sent: null
+      show: false
     }
+    this.handleClick = this.handleClick.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleTyping = this.handleTyping.bind(this)
   }
   
 
@@ -49,19 +90,17 @@ class ChatInput extends Component {
     }
   }
 
-  handleTyping() {
-    this.props.socket.emit('typing')
+  handleClick() {
+    this.setState({
+      show: !this.state.show
+    })
   }
 
   render() {
-    console.log('PhotoIcon =>', PhotoIcon)
+  let close = () => this.setState({ show: false});
     return (
     <div>
-    <span>
-    <PhotoIcon className="photo-icon" iconStyle={styles.iconStyle} color={orange500} hoverColor={blue500} >
-    </PhotoIcon>
-    </span>
-    <span>
+      <Button className="photo-upload" onClick={this.handleClick.bind(this)}></Button>
       <TextField
       className="input-chat"
       floatingLabelText="Start Chatting Here"
@@ -71,7 +110,26 @@ class ChatInput extends Component {
       underlineStyle={styles.underlineStyle}
       onKeyUp={this.handleSubmit}
       />
-    </span>
+        <Modal
+        show={this.state.show}
+        onHide={close}
+        container={this}
+        aria-labelledby="contained-modal-title">
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body>
+          <h2 style="text-align: center;">Upload Pictures from the Event!</h2>
+          <div className="row">
+            <img className="col-xs-6 col-xs-offset-3" src="" id="img-preview" />
+          </div>
+          <button className="btn btn-lg upload-btn col-xs-6 col-xs-offset-3" type="button">Upload From Computer</button>
+          <input className="col-xs-6 col-xs-offset-3" type="text" name="Link" placeholder="or add url" id="url-input" />
+          <button className="btn btn-sm col-xs-4 col-xs-offset-4" type="button">Back to Event Page</button>
+          <input id="upload-input" type="file" name="uploads[]" multiple="multiple" /><br />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button type="submit" text-align="center" onClick={this.handleSubmit}>Create Event</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
     )
   }
