7490fd4a317e944672aa1825826f9588c5f28ccf
joannawheeler <joannachristinewheeler@gmail.com>
2017-04-24 11:56:36 -0700

    deploy removed index bundle from git ignore

---
+++ app/components/foo.js
@@ -0,0 +1,23 @@
+
+
+import React from 'react';
+
+export class Foo extends React.Component {
+  render() {
+    return (
+      <div className="foo">
+
+        <div className="bar">
+        </div>
+
+        <div className="bar">
+        </div>
+
+      </div>
+
+
+    )
+  }
+}
+
+export default Foo;
