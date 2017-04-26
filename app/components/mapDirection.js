d9aa8d49f662b653b4ae75ab48ce3b606c9b45f0
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2017-04-26 00:54:52 -0700

    Added componentDidMount and directiondisplay

--- app/components/mapDirection.js
+++ app/components/mapDirection.js
@@ -7,21 +7,31 @@ class MapDirection extends Component {
     };
   }
 
+  componentDidMount() {
+    console.log('hiiii')
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
+
+    directionsDisplay.setPanel(document.getElementById('right-panel'));
+  }
+    
 
 
 
-
-
-
+  
   render() {
     console.log("clickedEvent clickedddd", this.props.clickedEvent)
     if (this.props.clickedEvent) {
       return (
-        <div><b>Direction to {this.props.clickedEvent['title']}</b></div>
+        <div>
+          <div><b>Direction to {this.props.clickedEvent['title']}</b></div>
+          <div id="right-panel"></div>
+        </div>
+
       )
     }
     return (
-      <div>helo</div>
+      <div></div>
     )
   }
 }
