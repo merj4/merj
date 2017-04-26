562e53dce5f00e7f760d745c6f477836729a2aa9
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2017-04-26 03:19:39 -0700

    can get event direction

--- app/components/mapDirection.js
+++ app/components/mapDirection.js
@@ -1,49 +1,40 @@
 import React, {Component} from 'react';
 
 class MapDirection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
   componentDidMount() {
-    const directionsDisplay = new google.maps.DirectionsRenderer;
-    const directionsService = new google.maps.DirectionsService;
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer;
+    this.directionsDisplay.setPanel(this.refs.directions);
+  }
+  componentDidUpdate() {
+    const start = this.props.clickedEvent['location'];
+    const end = this.props.userlocation;
 
-    directionsDisplay.setPanel(document.getElementById('right-panel'));
-    const control = document.getElementById('floating-panel');
-
-    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-      const start = this.props.clickedEvent['location'];
-      const index = this.props.userlocations.length -1;
-      const end = this.props.userlocations[index];
-
-      directionsService.route({
-        origin: start,
-        destination: end,
-        travelMode: 'DRIVING'
+    if (end) {
+      this.directionsService.route({
+      origin: start,
+      destination: end,
+      travelMode: 'DRIVING'
       }, (response, status) => {
-        if (status === 'OK') {
-          directionsDisplay.setDirections(response);
-        } else {
-          window.alert('Directions request failed due to ' + status);
-        }
+      if (status === 'OK') {
+        this.directionsDisplay.setDirections(response);
+        console.log('response', response)
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
       });
+    } else {
+      console.log('not yet!')
     }
   }
 
   render() {
-    
-
     return (
       <div>
-        <div id="floating-panel"></div>
-        <div id="right-panel"></div>
+        <div ref="directions"></div>
       </div>
     )
   }
 }
-
 
 export {MapDirection};
