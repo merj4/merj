cfdc7f301ad7079b627f5751277799d992e38c68
Rochelle Valdez <rochellevee@gmail.com>
2017-04-25 22:25:56 -0700

    adjusting width of dropdown

--- app/components/chat-giphys.js
+++ app/components/chat-giphys.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 import { GridList } from 'material-ui/GridList';
-import { gif } from './chat-gif'
+// import { Gif } from './chat-gif'
+import { GridTile } from 'material-ui/GridList';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import Divider from 'material-ui/Divider';
+import Download from 'material-ui/svg-icons/file/file-download';
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import axios from 'axios'
 
 //Giphy API
@@ -14,6 +22,8 @@ const styles = {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
+    maxWidth: 70 + 'em',
+    maxHeight: 40 + 'em'
   },
   gridList: {
     display: 'flex',
@@ -26,8 +36,37 @@ const styles = {
     left: 2 + "em",
     visible: "hidden"
   },
+  titleStyle: {
+    color: 'rgb(0, 188, 212)'
+  }
 };
-
+const tilesData = [
+  {
+    img: "http://media3.giphy.com/media/3o6ZsT1OxqTBHiZXuU/giphy-downsized.gif",
+    title: 'Breakfast',
+    author: 'jill111',
+  },
+  {
+    img: './images/jesse.jpg',
+    title: 'Tasty burger',
+    author: 'pashminu',
+  },
+  {
+    img: './images/faiz.jpg',
+    title: 'Camera',
+    author: 'Danson67',
+  },
+  {
+    img: './images/simon.jpg',
+    title: 'Morning',
+    author: 'fancycrave1',
+  },
+  {
+    img: './images/jordan.jpg',
+    title: 'Hats',
+    author: 'Hans',
+  },
+];
 //Start of Gifs component
 class Gifs extends Component {
   constructor(props) {
@@ -40,22 +79,7 @@ class Gifs extends Component {
     this.closeDropdown = this.closeDropdown.bind(this)
     this.openDropdown = this.openDropdown.bind(this)
     this.handleQuery = this.handleQuery.bind(this)
-  }
-//API call to Giphy
-  componentDidUpdate() {
-
-    let url = api + apiKey + "&q=" + this.state.query;
-    test('URL', url)
-    axios.get(url)
-      .then(res => {
-       console.log('Get request successful', res)
-        this.setState({gifs: res.data.data})
-      })
-      .catch(err => {
-        console.log('GET FAIL', err)
-      })
-    //Open dropdown with search results
-    this.openDropdown()
+    this.getGifs = this.getGifs.bind(this)
   }
 
 //Opens dropdown
@@ -72,34 +96,47 @@ class Gifs extends Component {
   handleQuery(query) {
     query = query.slice(4);
     this.setState({query: query})
+    this.getGifs()
   }
 
+//API call to Giphy
+  getGifs() {
+    let url = api + apiKey + "&q=" + this.state.query;
+    test('URL', url)
+    axios.get(url)
+      .then(res => {
+       console.log('Get request successful', res)
+        this.setState({gifs: res.data.data})
+      })
+      .catch(err => {
+        test('GET FAIL', err)
+      })
+    //Open dropdown with search results
+    this.openDropdown()
+  }
 
 //Dropdown serves as container and iterator for array of gif results
   render() {
-    test('Drop', this.state.dropdown) 
-    test('query', this.state.query)
-    console.log('GIFS ARRAY', this.state.gifs)
-      return (
+  return (
     <IconMenu
       style={styles.icon}
       open={this.state.dropdown}
       iconButtonElement={<div></div>}
-      anchorOrigin={{vertical: 'bottom'}}
-      targetOrigin={{vertical: 'bottom'}}
+      anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+      autoWidth={false}
+      className={''}
       onClick={this.closeDropdown}
     >
       <div style={styles.root}>
-        <GridList style={styles.gridList} cols={2.2}>
-          {this.state.gifs.map((gif, i) => (
-            <gif url={gif.bitly_url} key={i} 
-            handleDropdown={this.openDropdown}
-            insertURL={this.props.insertURL}
-            />
+        <GridList style={styles.gridList} 
+        >
+        {this.state.gifs.map((gif, i) => (
+          <GridTile key={i}>
+            <img src={gif.images.downsized_large.url} />
+          </GridTile>          
           ))}
         </GridList>
       </div>
-
     </IconMenu>
     );
   }
