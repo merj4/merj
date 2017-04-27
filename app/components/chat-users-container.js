282ed3212996353eefc2a90a16d6b8fd94955961
Rochelle Valdez <rochellevee@gmail.com>
2017-04-27 11:56:17 -0700

    chat-users-container: replaced MobileTearSheet with Paper, code cleanup

--- app/components/chat-users-container.js
+++ app/components/chat-users-container.js
@@ -3,53 +3,24 @@ import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import { ChatUsers } from './chat-users';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import Paper from 'material-ui/Paper';
 
 
-let MobileTearSheet = React.createClass({
-
-  propTypes: {
-    height: React.PropTypes.number
+let styles = {
+  paper: {
+    float: 'left',
+    marginBottom: 24,
+    marginRight: 24,
+    width: 360,
+    border: 'solid 1px #d9d9d9',
+    height: 350,
+    overflow: 'hidden'
   },
-
-  getDefaultProps() {
-    return {
-      height: 350
-    };
-  },
-
-  render() {
-
-    let styles = {
-      root: {
-        float: 'left',
-        marginBottom: 24,
-        marginRight: 24,
-        width: 360
-
-      },
-
-      container: {
-        border: 'solid 1px #d9d9d9',
-        height: this.props.height,
-        overflow: 'hidden'
-      }
-    };
-
-    return (
-      <div style={styles.root}>
-        <div style={styles.container}>
-          {this.props.children}
-        </div>
-      </div>
-    );
-  }
-
-});
+};
 
 const UsersContainer = (props) => {
-  // console.log('UsersContainer: ', props)
   return (
-    <MobileTearSheet >
+    <Paper zDepth={1} style={styles.paper} > 
       <Subheader>Attendees</Subheader>
       <InfiniteScroll
       height={775}
@@ -64,7 +35,7 @@ const UsersContainer = (props) => {
         )}
         </List>
       </InfiniteScroll>
-    </MobileTearSheet>
+    </Paper>
   )
 }
 
