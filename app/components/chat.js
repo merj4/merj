79c325fddad8bd489b4bc1ebf0b15c3865caebbf
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2017-04-27 21:23:28 -0700

    deleted chat.js

--- app/components/chat.js
+++
@@ -1,6 +0,0 @@
-// every class must have a render function
-  // it's recommended that you start with a functional based component and
-  // only refactor to a class when you need some added functionality
-
-  // This component will control the chat, which we may use socket.io for
-  // This (probably class component) component will be passed into the eventView controller
