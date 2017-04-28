3f31769eb8e5876c9db1388538f559182fdbf95d
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2017-04-28 03:22:47 -0700

    Added about us and pictures, modals

--- app/components/login.js
+++ app/components/login.js
@@ -33,37 +33,47 @@ export class Login extends React.Component {
             onHide={close}
             container={this}
             aria-labelledby="contained-modal-title">
-            <a target="_blank" href="https://github.com/merj4/merj" >
-            <Modal.Header closeButton>We are Team merj</Modal.Header>
-            </a>
+            <Modal.Header closeButton id="merj">We are Team merj</Modal.Header>
 
             <Modal.Body>
             <div className="team">
-            <a target="_blank" href="https://www.linkedin.com/in/emmavrios/" />
               <img id="teampic" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/0a2/329/026f9ab.jpg" />
-              <span className="name">Ema Rios</span>
-              <span>　　　Product Owner</span>
+              <span className="name">Ema Rios　</span>
+              <span id="role">　　Product Owner 
+              <a target="_blank" href="https://www.linkedin.com/in/emmavrios/" />
+              <img id="links" src="./images/linkedin.png" />
+              <img id="links" src="./images/github.png" />
+              </span>
             </div>
 
             <div className="team">
-            <a target="_blank" href="https://www.linkedin.com/in/misakimatsumoto/" />
               <img  id="teampic" src="https://avatars2.githubusercontent.com/u/8436387?v=3&s=400" />
               <span className="name">Misaki Matsumoto</span>
-              <span>　　　Scrum Master</span>
+              <span id="role">Scrum Master
+              <img id="links" src="./images/linkedin.png" />
+              <img id="links" src="./images/github.png" />
+              <a target="_blank" href="https://www.linkedin.com/in/misakimatsumoto/" />
+              </span>
             </div>
 
-          <a target="_blank" href="https://www.linkedin.com/in/rochelle-v/" />
             <div className="team">
               <img id="teampic" src="https://avatars1.githubusercontent.com/u/23320281?v=3&s=400" />
               <span className="name">Rochelle Valdez</span>
-              <span>　　　Senior Fullstack Engineer</span>
+              <span id="role">  Senior Fullstack Engineer
+              <img id="links" src="./images/linkedin.png" />
+              <img id="links" src="./images/github.png" />
+              <a target="_blank" href="https://www.linkedin.com/in/rochelle-v/" />
+              </span>
             </div>
 
-          <a target="_blank" href="https://www.linkedin.com/in/joanna-wheeler/" />
             <div className="team">
               <img id="teampic" src="https://avatars1.githubusercontent.com/u/23461229?v=3&s=400" />
-            <span className="name">Joanna Wheeler</span>
-              <span>　　　Fullstack Engineer</span>
+            <span className="name">  Joanna Wheeler</span>
+              <span id="role">Fullstack Engineer
+              <img id="links" src="./images/linkedin.png" />
+              <img id="links" src="./images/github.png" />
+              <a target="_blank" href="https://www.linkedin.com/in/joanna-wheeler/" />
+              </span>
             </div>
 
             </Modal.Body>
